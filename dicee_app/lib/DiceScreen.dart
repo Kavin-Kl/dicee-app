22f2e7f52078420b3a0b8f2374c4aa569bef44a9
Kavin <kavinkl20@gmail.com>
2025-02-21 01:20:52 +0530

    fin

--- dicee_app/lib/DiceScreen.dart
+++ dicee_app/lib/DiceScreen.dart
@@ -1,4 +1,6 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class DiceScreen extends StatefulWidget {
   const DiceScreen({super.key});
@@ -9,6 +11,24 @@ class DiceScreen extends StatefulWidget {
 
 class _DiceScreenState extends State<DiceScreen> {
   @override
+  final AudioPlayer audioPlayer = AudioPlayer();
+  int ld = 1;
+  int rd = 1;
+
+  void rollDice() async {
+    setState(() {
+      ld = 1 + Random().nextInt(6);
+      rd = 1 + Random().nextInt(6);
+    });
+    await audioPlayer.play(AssetSource('audios/dice_roll.mp3'));
+  }
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 26, 27, 38),
@@ -28,16 +48,22 @@ class _DiceScreenState extends State<DiceScreen> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              'images/dice1.png',
-              width: 200,
-              height: 200,
+            GestureDetector(
+              onTap: rollDice,
+              child: Image.asset(
+                'images/dice$ld.png',
+                width: 200,
+                height: 200,
+              ),
             ),
             SizedBox(width: 20),
-            Image.asset(
-              'images/dice2.png',
-              width: 200,
-              height: 200,
+            GestureDetector(
+              onTap: rollDice,
+              child: Image.asset(
+                'images/dice$rd.png',
+                width: 200,
+                height: 200,
+              ),
             ),
           ],
         ),
